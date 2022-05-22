62704113f82cf71fb2cd445d9e09d74f1677d431
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2022-05-22 20:12:33 +0300

    add nice to meet you

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 3. Спросите имя пользователя с помощью метода **prompt**. Выведите сообщение на страницу с помощью **`document.write`** : **“Рад знакомству {имя пользователя}!”**.
 
 let askName = prompt("What is your name?");
-let greeting = `Hello ${askName} My name is Kubanycbek Tursunbekov welcome to my site.`;
+let greeting = `Hello ${askName} My name is Kubanycbek Tursunbekov nice to meet you.`;
 
 document.write(greeting);
 
